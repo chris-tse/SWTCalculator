bb84a7784013519a8857bea7c7b9624ad13cbb6f
Christopher Tse <christopher-tse@outlook.com>
2017-02-28 21:39:11 -0600

    Complete expression evaluation method

--- src/model/Model.java
+++ src/model/Model.java
@@ -3,6 +3,9 @@ package model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.regex.Pattern;
+
+import static java.lang.Math.pow;
 
 /**
  * Created by christophertse on 2017/02/23.
@@ -19,11 +22,6 @@ public class Model {
         splitString();
     }
 
-    public boolean isZero() {
-        if (exp.equals("0")) return true;
-        return false;
-    }
-
     public void input(String input) {
         if (exp.equals("0")) {
             System.out.println("Was zero");
@@ -31,36 +29,100 @@ public class Model {
             exp = input;
         }
         else exp += input;
-        splitString();
         notifyNumberListener();
         System.out.println("Current exp: " + exp);
     }
 
-    public double eval() {
-        return 0;
+    public void del() {
+        if (exp != null && exp.length() > 1) {
+            exp = exp.substring(0, exp.length()-1);
+        } else {
+            exp = "0";
+        }
+
+        notifyNumberListener();
+    }
+
+    public void eval() {
+        splitString();
+        if(!isValid()) {
+            notifyInvalidListener();
+        } else {
+            splitString();
+            int index;
+            double result = 0;
+            System.out.println(exp);
+            while (splitList.contains("^")) {
+                index = splitList.indexOf("^");
+                result = Math.pow(Double.parseDouble(splitList.get(index-1)), Double.parseDouble(splitList.get(index+1)));
+                splitList.set(index - 1, String.valueOf(result));
+                splitList.remove(index);
+                splitList.remove(index);
+                System.out.println(splitList.toString());
+            }
+
+            while (splitList.contains("/")) {
+                index = splitList.indexOf("/");
+                result = Double.parseDouble(splitList.get(index-1)) / Double.parseDouble(splitList.get(index+1));
+                splitList.set(index - 1, String.valueOf(result));
+                splitList.remove(index);
+                splitList.remove(index);
+                System.out.println(splitList.toString());
+            }
+
+            while (splitList.contains("*")) {
+                index = splitList.indexOf("*");
+                result = Double.parseDouble(splitList.get(index-1)) * Double.parseDouble(splitList.get(index+1));
+                splitList.set(index - 1, String.valueOf(result));
+                splitList.remove(index);
+                splitList.remove(index);
+                System.out.println(splitList.toString());
+            }
+
+            while (splitList.contains("-")) {
+                index = splitList.indexOf("-");
+                result = Double.parseDouble(splitList.get(index-1)) - Double.parseDouble(splitList.get(index+1));
+                splitList.set(index - 1, String.valueOf(result));
+                splitList.remove(index);
+                splitList.remove(index);
+                System.out.println(splitList.toString());
+            }
+
+            while (splitList.contains("+")) {
+                index = splitList.indexOf("+");
+                result = Double.parseDouble(splitList.get(index-1)) + Double.parseDouble(splitList.get(index+1));
+                splitList.set(index - 1, String.valueOf(result));
+                splitList.remove(index);
+                splitList.remove(index);
+                System.out.println(splitList.toString());
+            }
+
+            exp = String.valueOf(result);
+            notifyNumberListener();
+        }
     }
 
     public boolean isValid() {
         for (String str : split) {
             if (str.matches("[-+*^/]{2,}")) return false;
         }
-        if(split[0].matches("[-+*^/]+") || split[split.length - 1].matches("[-+*^/]+")) return false;
+        if(split[0].matches("[-+*^/]+") || split[split.length - 1].matches("[-+.*^/]+")) return false;
         return true;
     }
 
     public void debug() {
+        splitString();
         System.out.println(splitList.toString());
         System.out.println(isValid());
     }
 
     public void clear() {
         exp = "0";
-        splitString();
         notifyNumberListener();
     }
 
-    public void memSet(String input) {
-        mem = input;
+    public void memSet() {
+        mem = exp;
     }
 
     public void getMem() {
@@ -69,7 +131,7 @@ public class Model {
 
     private void splitString() {
         splitList.clear();
-        split = exp.split("(?<=\\d)(?=\\D)|(?<=\\D)(?=\\d)");
+        split = exp.split("(?<=[-+*/()])|(?=[-+*/()])");
         Collections.addAll(splitList, split);
     }
 
@@ -83,4 +145,13 @@ public class Model {
             listener.numberChanged(exp);
         }
     }
+
+    public void notifyInvalidListener() {
+        //System.out.println("Listener called");
+        for(Listener listener : listeners ) {
+            listener.invalidCompute();
+        }
+    }
+
+
 }
